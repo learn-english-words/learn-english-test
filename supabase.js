3cb24cabd13df9c7deb963aba938a57fda04d1ed
english-words <sultanbnmadhiwsm@gmail.com>
2026-08-10 13:25:45 +0300

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 
-const SUPABASE_URL = "https://phxppigwlwcqsqyqrtbb.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://phxppigwlwcqsqyqrtbb.supabase.co";
 const SUPABASE_KEY = "sb_publishable_bggY0idi_eBSlEiE2rU3EQ_pxaJU39z";
 
 const supabaseClient = window.supabase.createClient(
